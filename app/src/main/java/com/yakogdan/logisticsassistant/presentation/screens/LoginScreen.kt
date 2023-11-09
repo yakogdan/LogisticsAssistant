1130187bf701cbf0bf7ab1022723cf391f6f6d47
yakogdan <yakogdan@gmail.com>
2023-11-10 00:03:53 +0800

    Поменяны некоторые цвета в теме. Установлен шрифт по умолчанию. Частично сделан экран LoginScreen.

--- app/src/main/java/com/yakogdan/logisticsassistant/presentation/screens/LoginScreen.kt
+++ app/src/main/java/com/yakogdan/logisticsassistant/presentation/screens/LoginScreen.kt
@@ -1,11 +1,107 @@
 package com.yakogdan.logisticsassistant.presentation.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.yakogdan.logisticsassistant.R
 
 @Composable
 fun LoginScreen() {
-    Text(text = "Login", fontSize = 30.sp)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+        Header()
+        Spacer(modifier = Modifier.height(76.dp))
+        Center()
+    }
+}
+
+@Composable
+private fun Header() {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_logo),
+            contentDescription = stringResource(
+                R.string.company_logo
+            )
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        Text(
+            text = stringResource(id = R.string.app_name),
+            fontSize = 16.sp,
+            fontFamily = FontFamily(
+                Font(R.font.stolzl_bold)
+            ),
+            color = MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun Center() {
+    Text(
+        text = stringResource(R.string.welcome_text),
+        fontSize = 22.sp,
+        fontFamily = FontFamily(Font(R.font.stolzl_regular)),
+        color = MaterialTheme.colorScheme.onBackground
+    )
+    Spacer(modifier = Modifier.height(16.dp))
+    Text(
+        text = stringResource(R.string.enter_number),
+        fontSize = 14.sp,
+        fontFamily = FontFamily(Font(R.font.stolzl_book)),
+        color = MaterialTheme.colorScheme.secondary
+    )
+    Spacer(modifier = Modifier.height(24.dp))
+    val textValue = remember {
+        mutableStateOf("")
+    }
+    OutlinedTextField(
+        modifier = Modifier.fillMaxWidth(),
+        value = textValue.value,
+        onValueChange = {
+            textValue.value = it
+        },
+        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Phone)
+    )
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    Box(modifier = Modifier.background(Color.White)) {
+        LoginScreen()
+    }
 }
 
--- app/src/main/java/com/yakogdan/logisticsassistant/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/yakogdan/logisticsassistant/presentation/ui/theme/Theme.kt
@@ -10,31 +10,36 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+private val darkColorScheme = darkColorScheme(
+    primary = Color(0xFF000000),
+    secondary = DarkGray,
+    tertiary = Pink80,
+    background = Color(0xFF000000),
+    surface = Color(0xFF000000),
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
+    onBackground = Color.White,
+    onSurface = Color(0xFFE9E4F5),
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+private val lightColorScheme = lightColorScheme(
+    primary = Color.White,
+    secondary = DarkGray,
+    tertiary = Pink40,
+    background = Color.White,
+    surface = Color(0xFFCECECE),
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
+    onBackground = Black,
     onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
@@ -50,8 +55,8 @@ fun LogisticsAssistantTheme(
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
