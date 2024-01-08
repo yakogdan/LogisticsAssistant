de91d1b282e48faf94195e60f0432c15a0fb245e
yakogdan <yakogdan@gmail.com>
2024-01-08 17:33:24 +0800

    Добавлен bottomBar для главного экрана.

--- app/src/main/java/com/yakogdan/logisticsassistant/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/yakogdan/logisticsassistant/presentation/screens/MainScreen.kt
@@ -1,9 +1,85 @@
 package com.yakogdan.logisticsassistant.presentation.screens
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.DateRange
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
 
 @Composable
 fun MainScreen() {
-    Text(text = "Main Screen")
+    val items = listOf(
+        BottomNavItem(
+            title = "Задания",
+            selectedIcon = Icons.Filled.CheckCircle,
+            unselectedIcon = Icons.Outlined.CheckCircle
+        ),
+        BottomNavItem(
+            title = "Графики",
+            selectedIcon = Icons.Filled.DateRange,
+            unselectedIcon = Icons.Outlined.DateRange
+        ),
+        BottomNavItem(
+            title = "Чат",
+            selectedIcon = Icons.Filled.Email,
+            unselectedIcon = Icons.Outlined.Email
+        ),
+        BottomNavItem(
+            title = "Профиль",
+            selectedIcon = Icons.Filled.Person,
+            unselectedIcon = Icons.Outlined.Person
+        ),
+    )
+
+    var selectedItemIndex by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+    Scaffold(
+        bottomBar = {
+            NavigationBar {
+                items.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        selected = selectedItemIndex == index,
+                        onClick = { selectedItemIndex = index },
+                        label = { Text(text = item.title)},
+                        icon = {
+                            Icon(
+                                imageVector = if (index == selectedItemIndex) {
+                                    item.selectedIcon
+                                } else {
+                                    item.unselectedIcon
+                                },
+                                contentDescription = item.title
+                            )
+                        }
+                    )
+                }
+            }
+        }
+    ) {
+        Text(text = "Main Screen", Modifier.padding(it))
+    }
 }
